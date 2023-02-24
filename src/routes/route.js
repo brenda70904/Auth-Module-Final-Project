401057294ededb34804b8019fe5fa69068d3f69b
Brenda Jow <brendaj0214@gmail.com>
2023-02-23 16:00:46 -0800

    working on route

---
+++ src/routes/route.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require("express");
+const userModel = require("../models/users");
+const Router = express.Router();
+const dataModules = reuqiure('./models');
+
+Router.param('model', (req, res, next) => {
+  const modelName = req.params.model;
+  if (dataModules[modelName]) {
+    req.model = dataModules[modelName];
+  } else {
+    next('invalid model')
+  };
+});
+
+Router.post('/signup', async (req, res, next)=>{
+  try{
+    let record = await userModel(req.body);
+  }catch(e){
+
+  }
+})
